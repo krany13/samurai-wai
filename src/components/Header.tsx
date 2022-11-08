3fc2065ffa4eb7e9b7db795d5f9bfe832c4970dd
krany13 <krasniy.vladim@yandex.ru>
2022-11-09 00:51:06 +0300

    components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className={"header"}>
+            <img src="https://cdn-icons-png.flaticon.com/512/5270/5270995.png" alt=""/>
+        </header>
+    );
+};
---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={"content"}>
+            <div><img src="https://static.tildacdn.com/tild3939-3635-4064-a335-386461613361/social-network-app.png"
+                      alt=""/></div>
+            <div>ava + descr</div>
+            <div>
+                My Posts
+                <div>New Post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
