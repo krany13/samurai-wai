641c2a2d72471604a29c16a1e6bb8348b68eebe3
krany13 <krasniy.vladim@yandex.ru>
2022-11-11 09:27:16 +0300

    new components

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div><img src="https://static.tildacdn.com/tild3939-3635-4064-a335-386461613361/social-network-app.png"
                       alt=""/></div>
             <div>
